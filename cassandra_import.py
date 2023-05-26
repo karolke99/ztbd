eee7ed9e906fc4061f3292e97e1f115e0598f620
karolke99 <karolke99@gmail.com>
2023-05-26 18:36:02 +0200

    cassandra fixes

--- cassandra_import.py
+++ cassandra_import.py
@@ -1,70 +1,216 @@
-## TODO
 from cassandra.cluster import Cluster
 import csv
 import pandas as pd
 
 cluster = Cluster(['localhost'], port = 9042)
-
-keyspace_name = 'mubi'
-replication_options = {
-    'class': 'SimpleStrategy',
-    'replication_factor': 3
-}
-query = f"CREATE KEYSPACE IF NOT EXISTS {keyspace_name} WITH REPLICATION = {replication_options}"
+keyspace_name = 'ztbd'
 
 session = cluster.connect()
-session.execute(query)
 session.set_keyspace(keyspace_name)
 
-table_name = 'ratings'
-# insert_query = f"INSERT INTO {table_name} (movie_id, movie_title, movie_release_year, movie_url, movie_title_language,"\
-#                f" movie_popularity, movie_image_url, director_id, director_name, director_url) " \
-#                f"VALUES (?,?,?,?,?,?,?,?,?,?)"
+# CREATE TABLE lists_data (
+#     user_id int,
+#     list_id int,
+#     list_title text,
+#     list_movie_number text,
+#     list_update_timestamp_utc text,
+#     list_creation_timestamp_utc text,
+#     list_followers text,
+#     list_url text,
+#     list_comments text,
+#     list_description text,
+#     list_cover_image_url text,
+#     list_first_image_url text,
+#     list_second_image_url text,
+#     list_third_image_url text,
+#     PRIMARY KEY (list_id)
+# )
 
-insert_query = f"INSERT INTO {table_name} (movie_id, rating_id, rating_url, raitng_score, rating_timestamp_utc, critic," \
-               f" critic_likes, critic_comments, user_id, user_trialist, user_subscriber, user_eligible_for_trial," \
-               f" user_has_payment_method) " \
-               f"VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"
+# CREATE TABLE lists_user_data (
+#     user_id text,
+#     list_id text,
+#     list_update_date_utc text,
+#     list_creation_date_utc text,
+#     user_trialist text,
+#     user_subscriber text,
+#     user_avatar_image_url text,
+#     user_cover_image_url text,
+#     user_eligible_for_trial text,
+#     user_has_payment_method text,
+#     PRIMARY KEY(user_id)
+# )
 
-df = pd.read_csv('./data/mubi_ratings_data.csv')
-data = df.values.tolist()
-session.execute(insert_query, data)
-...
+# CREATE TABLE movie_data(
+#     movie_id text,
+#     movie_title text,
+#     movie_release_year text,
+#     movie_url text,
+#     movie_title_language text,
+#     movie_popularity text,
+#     movie_image_url text,
+#     director_id text,
+#     director_name text,
+#     director_url text,
+#     PRIMARY KEY(movie_id)
+# )
 
+# CREATE TABLE ratings_data (
+#     movie_id text,
+#     rating_id text,
+#     rating_url text,
+#     rating_score text,
+#     rating_timestamp_utc text,
+#     critic text,
+#     critic_likes text,
+#     critic_comments text,
+#     user_id text,
+#     user_trialist text,
+#     user_subscriber text,
+#     user_eligible_for_trial text,
+#     user_has_payment_method text,
+#     PRIMARY KEY(rating_id)
+# )
 
+# CREATE TABLE ratings_user_data (
+#     user_id text,
+#     rating_date_utc text,
+#     user_trialist text,
+#     user_subscriber text,
+#     user_avatar_image_url text,
+#     user_cover_image_url text,
+#     user_eligible_for_trial text,
+#     user_has_payment_method text,
+#     PRIMARY KEY(user_id)
+# )
 
+with open('archive/mubi_lists_data.csv') as csvfile:
+    csvreader = csv.reader(csvfile)
 
+    next(csvreader)
 
+    for row in csvreader:
+        session.execute(
+            f"""
+            INSERT INTO lists_data (user_id, 
+                list_id,
+                list_title, 
+                list_movie_number, 
+                list_update_timestamp_utc, 
+                list_creation_timestamp_utc, 
+                list_followers, 
+                list_url,
+                list_comments,
+                list_description,
+                list_cover_image_url,
+                list_first_image_url,
+                list_second_image_url,
+                list_third_image_url
+                )
+            VALUES ({int(row[0])} ,{int(row[1])} , %s, %s, %s ,%s , %s, %s, %s ,%s , %s, %s, %s, %s)
+            """,
+            (row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13])
+        )
 
+with open('archive/mubi_lists_user_data.csv') as csvfile:
+    csvreader = csv.reader(csvfile)
 
+    next(csvreader)
 
+    for row in csvreader:
+        session.execute(
+            """
+            INSERT INTO lists_user_data (
+                user_id,
+                list_id,
+                list_update_date_utc,
+                list_creation_date_utc,
+                user_trialist,
+                user_subscriber,
+                user_avatar_image_url,
+                user_cover_image_url,
+                user_eligible_for_trial,
+                user_has_payment_method
+                )
+            VALUES (%s ,%s , %s, %s, %s ,%s , %s, %s, %s ,%s)
+            """,
+            (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9])
+        )
 
+with open('archive/mubi_movie_data.csv') as csvfile:
+    csvreader = csv.reader(csvfile)
 
+    next(csvreader)
 
+    for row in csvreader:
+        session.execute(
+            """
+            INSERT INTO movie_data (
+                movie_id,
+                movie_title,
+                movie_release_year,
+                movie_url,
+                movie_title_language,
+                movie_popularity,
+                movie_image_url,
+                director_id,
+                director_name,
+                director_url
+                )
+            VALUES (%s ,%s , %s, %s, %s ,%s , %s, %s, %s ,%s)
+            """,
+            (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9])
+        )
 
+with open('archive/mubi_ratings_data.csv') as csvfile:
+    csvreader = csv.reader(csvfile)
 
+    next(csvreader)
 
+    for row in csvreader:
+        session.execute(
+            """
+            INSERT INTO ratings_data (
+                movie_id,
+                rating_id,
+                rating_url,
+                rating_score,
+                rating_timestamp_utc,
+                critic,
+                critic_likes,
+                critic_comments,
+                user_id,
+                user_trialist,
+                user_subscriber,
+                user_eligible_for_trial,
+                user_has_payment_method
+                )
+            VALUES (%s ,%s , %s, %s, %s ,%s , %s, %s, %s , %s, %s, %s, %s)
+            """,
+            (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12])
+        )
 
+with open('archive/mubi_ratings_user_data.csv') as csvfile:
+    csvreader = csv.reader(csvfile)
 
+    next(csvreader)
 
-
-
-
-
-
-
-
-
-session.execute("CREATE KEYSPACE IF NOT EXISTS my_keyspace WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'}")
-session.execute("USE my_keyspace")
-session.execute("CREATE TABLE IF NOT EXISTS my_table (column1 datatype1, column2 datatype2, ...)")
-
-with open('path/to/your/csv_file.csv', 'r') as file:
-    csv_data = csv.reader(file)
-    next(csv_data)  # Skip the header row if present
-    for row in csv_data:
-        query = "INSERT INTO my_table (column1, column2, ...) VALUES (%s, %s, ...)"
-        session.execute(query, row)
+    for row in csvreader:
+        session.execute(
+            """
+            INSERT INTO ratings_user_data (
+                user_id,
+                rating_date_utc,
+                user_trialist,
+                user_subscriber,
+                user_avatar_image_url,
+                user_cover_image_url,
+                user_eligible_for_trial,
+                user_has_payment_method
+                )
+            VALUES (%s ,%s , %s, %s, %s ,%s , %s, %s)
+            """,
+            (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
+        )
 
 session.shutdown()
 cluster.shutdown()
--- cassandra_manager.py
+++ cassandra_manager.py
@@ -10,37 +10,47 @@ class CassandraManager:
 
     def __init__(self):
         self.cluster = Cluster(["0.0.0.0"], port=9042)
-        keyspace = 'mubi'
+        keyspace = 'ztbd'
         self.session = self.cluster.connect(keyspace)
 
     def select(self, rows_number=None):
-        query = f"SELECT * FROM {self.table_name}"
+        query = f"SELECT * FROM ratings_data"
         if rows_number is not None:
-            query = query + "limit " + str(rows_number)
+            query = query + " limit " + str(rows_number)
+
+        print(query)
         return self._execute(query)
 
     def _generate_insert_query(self):
-        values = (uuid.uuid4().int & 0xFFFFFFFF, uuid.uuid4().int & 0xFFFFFFFF,
+        values = ('12345', '12345',
                   'http://mubi.com/films/pavee-lackeen-the-traveller-girl/ratings/15610495', 3.0,
-                  '2017-06-10 12:38:33', 'Content', 0, 0, uuid.uuid4().int & 0xFFFFFFFF, False, False, True, False)
+                  '2017-06-10 12:38:33', 'Content', '0', '0', '123456676', 'False', 'False', 'True', 'False')
 
-        query = "INSERT INTO mubi_ratings_data (movie_id, rating_id, rating_url, rating_score, " \
+        query = "INSERT INTO ratings_data (movie_id, rating_id, rating_url, rating_scoree, " \
                 "rating_timestamp_utc, critic, critic_likes, critic_comments, user_id, user_trialist, " \
                 "user_subscriber, user_eligible_for_trial, user_has_payment_method) " \
-                f"VALUES ({values})"
+                f"VALUES {values}"
 
         return self.session.prepare(query)
 
-    def insert(self, rows_number):
+    def insert(self, rows_number=1):
         batch = BatchStatement(consistency_level=ConsistencyLevel.QUORUM)
         for _ in range(rows_number):
             query = self._generate_insert_query()
             batch.add(query)
         return self._execute(batch)
 
-    def update(self, rows_number):
-        query = f"UPDATE mubi_ratings_data SET rating_url = NULL WHERE user_id IN " \
-                f"( SELECT user_id FROM mubi_ratings_data LIMIT {rows_number});"
+    def update(self, rows_number=1):
+        
+        query = f"SELECT rating_id FROM ratings_data LIMIT {rows_number}"
+        rows = self.session.execute(query)
+        ids = []
+        for row in rows:
+            ids.append(f'\'{row.rating_id}\'')
+
+        query = f'UPDATE ratings_data SET rating_url = \'someth\' WHERE rating_id IN ' \
+                f'({", ".join(ids)});'
+        
         return self._execute(query)
 
     def _execute(self, query):
@@ -48,30 +58,49 @@ class CassandraManager:
         self.session.execute(query)
         return time.time() - start_time
 
-    def delete(self, rows_number):
-        query = f"DELETE from mubi_ratings_data WHERE user_id IN ( SELECT user_id FROM mubi_ratings_data LIMIT {rows_number});"
+    def delete(self, rows_number=1):
+        query = f"SELECT rating_id FROM ratings_data LIMIT {rows_number}"
+        rows = self.session.execute(query)
+        ids = []
+        for row in rows:
+            ids.append(f'\'{row.rating_id}\'')
+        
+        query = f"DELETE from ratings_data WHERE rating_id IN " \
+                f'({", ".join(ids)});'
         return self._execute(query)
 
     def avg(self):
-        query = "SELECT AVG(user_id) FROM mubi_ratings_data;"
+        query = "SELECT AVG(user_id) FROM lists_data;"
         return self._execute(query)
 
     def median(self):
-        query = 'SELECT percentile_disc(0.5) WITHIN GROUP (ORDER BY user_id) AS median FROM mubi_ratings_data;'
+        # query = 'SELECT percentile_disc(0.5) WITHIN GROUP (ORDER BY user_id) AS median FROM lists_data;'
+        query = "SELECT AVG(user_id) FROM lists_data;"
         return self._execute(query)
 
     def count(self):
-        query = 'SELECT count(*) FROM mubi_ratings_data'
+        query = 'SELECT count(*) FROM lists_data'
         return self._execute(query)
 
     def min(self):
-        query = "SELECT min(user_id) FROM mubi_ratings_data;"
+        query = "SELECT min(user_id) FROM lists_data;"
         return self._execute(query)
 
     def max(self):
-        query = "SELECT max(user_id) FROM mubi_ratings_data;"
+        query = "SELECT max(user_id) FROM lists_data;"
         return self._execute(query)
 
-    def count_by_word(self):
-        query = f"SELECT tokenCount(tokenize(critics, ' ')) AS word_count FROM mubi_ratings_data;"
-        return self._execute(query)
+    # def count_by_word(self):
+    #     query = f"SELECT tokenCount(tokenize(critic, ' ')) AS word_count FROM ratings_data;"
+    #     return self._execute(query)
+
+manager = CassandraManager()
+print(f'select: {manager.select(5000)}')
+print(f'insert: {manager.insert(5000)}')
+print(f'update: {manager.update(5000)}')
+print(f'delete: {manager.delete(5000)}')
+print(f'avg: {manager.avg()}')
+print(f'median: {manager.median()}')
+print(f'count: {manager.count()}')
+print(f'min: {manager.min()}')
+print(f'max: {manager.max()}')
